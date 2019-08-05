9fbdd800b880e95f7587b1ac9981bc57bde3d96a
Iti Bansal <46072448+bansaliti048@users.noreply.github.com>
2019-08-05 17:23:25 -0400

    update ResNet.py

--- code/ResNet.py
+++ code/ResNet.py
@@ -1,27 +1,21 @@
+__author__ = 'ITI BANSAL'
+
 import numpy as np
 from keras import layers
-from keras.layers import Input, Add, Dense, Activation, ZeroPadding2D, BatchNormalization, Flatten, Conv2D, AveragePooling2D, MaxPooling2D, GlobalMaxPooling2D
-from keras.models import Model, load_model
-from keras.preprocessing import image
-from keras.utils import layer_utils
-from keras.utils.data_utils import get_file
-from keras.applications.imagenet_utils import preprocess_input
-from keras.utils.vis_utils import model_to_dot
-from keras.utils import plot_model
+from keras.layers import Input, Dense, Activation, ZeroPadding2D, BatchNormalization, Flatten, Conv2D, AveragePooling2D, MaxPooling2D
+from keras.models import Model
 from keras.initializers import glorot_uniform
-import scipy.misc
 from sklearn.preprocessing import LabelEncoder
+from keras.preprocessing.image import ImageDataGenerator
+from keras import backend as K
 import sys
 import cv2
-import os
-import glob
-import re
 import h5py
 
 
 def identity_block(X, f, filters, stage, block):
     """
-    Implementation of the identity block as defined in Figure 3
+    Implementation of the identity block
 
     Arguments:
     X -- input tensor of shape (m, n_H_prev, n_W_prev, n_C_prev)
@@ -50,8 +44,6 @@ def identity_block(X, f, filters, stage, block):
     X = BatchNormalization(axis=3, name=bn_name_base + '2a')(X)
     X = Activation('relu')(X)
 
-    ### START CODE HERE ###
-
     # Second component of main path (≈3 lines)
     X = Conv2D(filters=F2, kernel_size=(f, f), strides=(1, 1), padding='same', name=conv_name_base + '2b',
                kernel_initializer=glorot_uniform(seed=0))(X)
@@ -67,14 +59,12 @@ def identity_block(X, f, filters, stage, block):
     X = layers.add([X, X_shortcut])
     X = Activation('relu')(X)
 
-    ### END CODE HERE ###
-
     return X
 
 
 def convolutional_block(X, f, filters, stage, block, s=2):
     """
-    Implementation of the convolutional block as defined in Figure 4
+    Implementation of the convolutional block
 
     Arguments:
     X -- input tensor of shape (m, n_H_prev, n_W_prev, n_C_prev)
@@ -105,8 +95,6 @@ def convolutional_block(X, f, filters, stage, block, s=2):
     X = BatchNormalization(axis=3, name=bn_name_base + '2a')(X)
     X = Activation('relu')(X)
 
-    ### START CODE HERE ###
-
     # Second component of main path (≈3 lines)
     X = Conv2D(F2, (f, f), strides=(1, 1), padding='same', name=conv_name_base + '2b',
                kernel_initializer=glorot_uniform(seed=0))(X)
@@ -126,8 +114,6 @@ def convolutional_block(X, f, filters, stage, block, s=2):
     # Final step: Add shortcut value to main path, and pass it through a RELU activation (≈2 lines)
     X = layers.add([X, X_shortcut])
     X = Activation('relu')(X)
-
-    ### END CODE HERE ###
 
     return X
 
@@ -187,8 +173,6 @@ def ResNet50(input_shape=(64, 64, 3), classes=6):
     # AVGPOOL (≈1 line). Use "X = AveragePooling2D(...)(X)"
     X = AveragePooling2D(pool_size=(2, 2), padding='same')(X)
 
-    ### END CODE HERE ###
-
     # output layer
     X = Flatten()(X)
     X = Dense(classes, activation='softmax', name='fc' + str(classes), kernel_initializer=glorot_uniform(seed=0))(X)
@@ -197,26 +181,6 @@ def ResNet50(input_shape=(64, 64, 3), classes=6):
     model = Model(inputs=X_input, outputs=X, name='ResNet50')
 
     return model
-
-def load_dataset():
-    train_dataset = h5py.File('/home/stu5/s16/ib6355/IndStudy/train_signs.h5', "r")
-    train_set_x_orig = np.array(train_dataset["train_set_x"][:])  # your train set features
-    train_set_y_orig = np.array(train_dataset["train_set_y"][:])  # your train set labels
-    test_dataset = h5py.File('/home/stu5/s16/ib6355/IndStudy/test_signs.h5', "r")
-    test_set_x_orig = np.array(test_dataset["test_set_x"][:])  # your test set features
-    test_set_y_orig = np.array(test_dataset["test_set_y"][:])  # your test set labels
-    train_x_orig = np.empty((len(train_set_x_orig), 224, 224, 3))
-    test_x_orig = np.empty((len(test_set_x_orig), 224, 224, 3))
-    for i in range(len(train_set_x_orig)):
-        train_x_orig[i, :] = cv2.resize(train_set_x_orig[i, :], (224, 224), interpolation=cv2.INTER_CUBIC)
-        # print(train_x_orig[i,:])
-    for i in range(len(test_set_x_orig)):
-        test_x_orig[i, :] = cv2.resize(test_set_x_orig[i, :], (224, 224), interpolation=cv2.INTER_CUBIC)
-        # print(test_x_orig[i,:])
-    classes = np.array(test_dataset["list_classes"][:])  # the list of classes
-    train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
-    test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
-    return train_x_orig, train_set_y_orig, test_x_orig, test_set_y_orig, classes
 
 def load_dataset(train_path,test_path):
     '''
@@ -227,8 +191,7 @@ def load_dataset(train_path,test_path):
     hdf5_file = h5py.File(train_path, "r")
     train_x_orig = np.array(hdf5_file["train_img"][:])
     train_y_orig = np.array(hdf5_file["train_labels"][:])
-    # train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
-    # Y_train = convert_to_one_hot(train_set_y_orig, 6).T
+
     labelencoder = LabelEncoder()
     train_y_orig = labelencoder.fit_transform(train_y_orig)
     train_y_orig = train_y_orig.reshape((1, train_y_orig.shape[0]))
@@ -238,8 +201,7 @@ def load_dataset(train_path,test_path):
     hdf5_file_test = h5py.File(test_path, "r")
     test_x_orig = np.array(hdf5_file_test["test_img"][:])
     test_y_orig = np.array(hdf5_file_test["test_labels"][:])
-    # train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
-    # Y_train = convert_to_one_hot(train_set_y_orig, 6).T
+
     labelencoder = LabelEncoder()
     test_y_orig = labelencoder.fit_transform(test_y_orig)
     test_y_orig = test_y_orig.reshape((1, test_y_orig.shape[0]))
@@ -249,25 +211,121 @@ def load_dataset(train_path,test_path):
     return x_train,y_train,x_test,y_test
 
 def convert_to_one_hot(Y, C):
+    '''
+    This function does the one hot encoding of the y labels
+    '''
     Y = np.eye(C)[Y.reshape(-1)].T
     return Y
 
-# X_train_orig, Y_train_orig, X_test_orig, Y_test_orig, classes = load_dataset()
-# # Normalize image vectors
-# X_train = X_train_orig/255
-# X_test = X_test_orig/255
-#
-# # Convert training and test labels to one hot matrices
-# Y_train = convert_to_one_hot(Y_train_orig, 6).T
-# Y_test = convert_to_one_hot(Y_test_orig, 6).T
 
-if __name__ == '__main__':
+def predict(train_data,test_data):
+    '''
+    This function predicts the labels when the images are not centered
+    '''
+    # Load the data
+    x_train, y_train, x_test, y_test = load_dataset(train_data, test_data)
+
+    # Train the model and save weights
     model = ResNet50(input_shape=(224, 224, 3), classes=36)
     model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-    x_train,y_train,x_test,y_test=load_dataset("training_data.hdf5","test_data.hdf5")
-    model.fit(x_test, y_test, epochs = 20, batch_size = 32)
-    preds = model.evaluate(x_train, y_train)
+    model.fit(x_train, y_train, epochs = 20, batch_size = 32)
+    model.save_weights("ResNetw1.h5")
+
+    # Evaluate the model
+    K.clear_session()
+    x_train, y_train, x_test, y_test = load_dataset(train_data, test_data)
+
+    # Train the model and save weights
+    K.set_learning_phase(1)
+    model = ResNet50(input_shape=(224, 224, 3), classes=36)
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    # model.fit(x_train, y_train, epochs = 10, batch_size = 32)
+    model.load_weights("ResNetw1.h5")
+    preds = model.evaluate(x_test, y_test)
     print ("Loss = " + str(preds[0]))
     print ("Test Accuracy = " + str(preds[1]))
 
 
+def predict_mean_image(train_data,test_data):
+    '''
+    This function predicts the labels when images are centered
+    '''
+    # Load the data
+    x_train, y_train, x_test, y_test = load_dataset(train_data, test_data)
+    datagen = ImageDataGenerator(featurewise_center=True)
+
+    # Calculate mean on training dataset
+    datagen.fit(x_train)
+
+    # Prepare an iterators to scale images
+    train_iterator = datagen.flow(x_train, y_train, batch_size=64)
+    #print('Batches train=%d, test=%d' % (len(train_iterator), len(test_iterator)))
+
+    # Train the nmodel
+    model = ResNet50(input_shape=(224, 224, 3), classes=36)
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    model.fit_generator(train_iterator, steps_per_epoch=len(train_iterator), epochs=20)
+    model.save_weights("ResNetw2.h5")
+
+    # Evaluate model
+    K.clear_session()
+    x_train, y_train, x_test, y_test = load_dataset(train_data, test_data)
+    K.set_learning_phase(1)
+    test_iterator = datagen.flow(x_test, y_test, batch_size=64)
+    model = ResNet50(input_shape=(224, 224, 3), classes=36)
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    model.load_weights("ResNetw2.h5")
+    _, acc = model.evaluate_generator(test_iterator, steps=len(test_iterator), verbose=0)
+    print('Test Accuracy: %.3f' % (acc * 100))
+
+def predict_normalize_image(train_data,test_data):
+    '''
+    This function predicts the labels with normalized images
+    '''
+    x_train, y_train, x_test, y_test = load_dataset(train_data, test_data)
+
+    model = ResNet50(input_shape=(224, 224, 3), classes=36)
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+    # Normalization
+    datagen = ImageDataGenerator(rescale=1.0 / 255.0)
+    train_iterator = datagen.flow(x_train, y_train, batch_size=64)
+    test_iterator = datagen.flow(x_test, y_test, batch_size=64)
+    print('Batches train=%d, test=%d' % (len(train_iterator), len(test_iterator)))
+    batchX, batchy = train_iterator.next()
+    print('Batch shape=%s, min=%.3f, max=%.3f' % (batchX.shape, batchX.min(), batchX.max()))
+
+    # Train and save the model
+    model.fit_generator(train_iterator, steps_per_epoch=len(train_iterator), epochs=20)
+    model.save_weights("ResNetw3.h5")
+
+    # Evaluate model
+    K.clear_session()
+    x_train, y_train, x_test, y_test = load_dataset(train_data, test_data)
+    K.set_learning_phase(1)
+    test_iterator = datagen.flow(x_test, y_test, batch_size=64)
+    model = ResNet50(input_shape=(224, 224, 3), classes=36)
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    model.load_weights("ResNetw3.h5")
+    _, acc = model.evaluate_generator(test_iterator, steps=len(test_iterator), verbose=0)
+    print('Test Accuracy: %.3f' % (acc * 100))
+
+if __name__ == '__main__':
+    try:
+        train_data=sys.argv[1]
+        test_data=sys.argv[2]
+        print("Please enter the image processing technique to use:"
+              "1) 1 for no preprocessing"
+              "2) 2 for centering the images"
+              "3) 3 for rescaling the images from [0-255] to [0-1]")
+        option = input("Please enter the option: ")
+        if option=="1":
+            predict(train_data,test_data)
+        elif option=="2":
+            predict_mean_image(train_data,test_data)
+        elif option=="3":
+            predict_normalize_image(train_data,test_data)
+        else:
+            print("Enter options 1/2/3")
+    except:
+        print("Usage: python3 ResNet.py train_data.hdf5 test_data.hdf5")
